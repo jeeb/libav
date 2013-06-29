cac0a096e5885eef91fc0074439808ad34258a1f
Guillaume Martres <smarter@ubuntu.com>
2013-06-29 15:00:34 +0200

    hevc: better checks for the beginning of an access unit

--- libavcodec/hevc_parser.c
+++ libavcodec/hevc_parser.c
@@ -39,7 +39,9 @@ static int hevc_find_frame_end(AVCodecParserContext *s, const uint8_t *buf, int 
         pc->state64 = (pc->state64 << 8) | buf[i];
         if (((pc->state64 >> 3*8) & 0xFFFFFF) == START_CODE) {
             int nut = (pc->state64 >> 2*8+1) & 0x3F;
-            if (nut >= NAL_VPS && nut <= NAL_AUD) { // Beginning of access unit
+            // Beginning of access unit
+            if (nut >= NAL_VPS && nut <= NAL_AUD || nut == NAL_SEI_PREFIX ||
+                nut >= 41 && nut <= 44 || nut >= 48 && nut <= 55) {
                 if (pc->frame_start_found) {
                     pc->frame_start_found = 0;
                     return i - 5;
