e78b21d8415720b5105cd8a281bb878438a4b149
Jan Ekström <jeebjp@gmail.com>
2015-06-28 01:51:23 +0300

    lavc: add Earthsoft DV id and description

--- libavcodec/codec_desc.c
+++ libavcodec/codec_desc.c
@@ -1364,6 +1364,13 @@ static const AVCodecDescriptor codec_descriptors[] = {
         .long_name = NULL_IF_CONFIG_SMALL("XWD (X Window Dump) image"),
         .props     = AV_CODEC_PROP_INTRA_ONLY | AV_CODEC_PROP_LOSSLESS,
     },
+    {
+        .id        = AV_CODEC_ID_EARTHSOFT_DV,
+        .type      = AVMEDIA_TYPE_VIDEO,
+        .name      = "earthsoft_dv",
+        .long_name = NULL_IF_CONFIG_SMALL("Earthsoft PV3/PV4 DV video codec"),
+        .props     = AV_CODEC_PROP_INTRA_ONLY | AV_CODEC_PROP_LOSSY,
+    },
 
     /* various PCM "codecs" */
     {
